3b38e996ec2945d53c29f6b597d2757a5d08e5f6
kavinkumar36365 <108183218+kavinkumar36365@users.noreply.github.com>
2024-02-20 13:44:14 +0530

    Update pdf_to_image.py
    update with the addition of dpi

--- pdf_to_image.py
+++ pdf_to_image.py
@@ -1,9 +1,16 @@
-import fitz  
+import fitz
 from PIL import Image
+
+# Define the DPI (dots per inch)
+dpi = 300  # You can adjust this value as needed
 
 pdf_document = fitz.open('sem3.pdf')
 
 page = pdf_document.load_page(0)
-pix = page.get_pixmap(alpha = False)    
+
+# Create the pixmap with a higher DPI
+pix = page.get_pixmap(matrix=fitz.Matrix(dpi / 72, dpi / 72))
+
+# Save the image
 image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-image.save("output.png")
+image.save("output1.png")
